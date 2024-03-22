a439fe80043daeede79d963b3f828308c095c197
Artem M <artmakatera@gmail.com>
2024-03-22 18:27:23 +0100

    draw binary tree

--- src/widgets/binary-tree/model/binary-tree.ts
+++ src/widgets/binary-tree/model/binary-tree.ts
@@ -14,6 +14,7 @@ class TreeNode {
 
 class BinarySearchTree {
   root: TreeNode | null;
+  count: number = 0;
 
   constructor() {
     this.root = null;
@@ -44,6 +45,8 @@ class BinarySearchTree {
         node = node.right;
       }
     }
+
+    this.count++;
 
   }
 
@@ -150,23 +153,87 @@ class BinarySearchTree {
     return node.value;
   }
 
-  addTreeNode({ current, result, isLeft, x = 400, y = 50, parent }: { current: TreeNode | null, result: any[], isLeft?: boolean, x?: number, y?: number, parent?: any }) {
+  getHeight(node: TreeNode | null = this.root): number {
+    if (node === null) {
+      return -1;
+    }
+
+    return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+  }
+
+  // addTreeNode({ current, result, isLeft, x = 400, y = 50, parent, level, height }: { current: TreeNode | null, result: any[], level: number, height: number, isLeft?: boolean, x?: number, y?: number, parent?: any, }) {
+  //   if (current) {
+  //     const data = { value: current.value, isLeft: !!isLeft, x, y, parent, level }
+  //     result.push(data);
+
+  //     let additional = height - level
+  //     this.addTreeNode({ current: current.left, result, x: x - 40 * additional, y: y + 60, isLeft: true, parent: data, level: level + 1, height });
+  //     this.addTreeNode({ current: current.right, result, x: x + 40 * additional, y: y + 60, parent: data, level: level + 1, height });
+  //   }
+
+  //   return result
+
+  // }
+
+  // getTreeView(x: number = 800) {
+
+  //   const result: any[] = [];
+  //   console.log(this.getHeight())
+  //   const height = this.getHeight();
+
+  //   return this.addTreeNode({ current: this.root, result, x: x, level: 0, height });
+  // }
+  addTreeNode({ current, result, isLeft, x = 400, y = 50, parent, level, height }: { current: TreeNode | null, result: any[], level: number, height: number, isLeft?: boolean, x?: number, y?: number, parent?: any, }) {
     if (current) {
-      const data = { value: current.value, isLeft: !!isLeft, x, y, parent }
+
+      let nextLevel = level - 1;
+
+      console.log({ height, level, nextLevel })
+      const step = 40
+      const radius = 20
+      const width = Math.pow(2, height) * (step + radius)
+      console.log({ width, level })
+      const data = { value: current.value, isLeft: !!isLeft, x, y, parent, level }
       result.push(data);
-      this.addTreeNode({ current: current.left, result, x: x - 50, y: y + 70, isLeft: true, parent: data });
-      this.addTreeNode({ current: current.right, result, x: x + 50, y: y + 70, parent: data });
+
+
+
+
+
+
+      this.addTreeNode({
+        current: current.left,
+        result, isLeft: true,
+        parent: data,
+        level: nextLevel,
+        height,
+        y: (y + 60),
+        x: x - step * Math.pow(2, nextLevel)
+      });
+      this.addTreeNode({
+        current: current.right,
+        result,
+        parent: data,
+        level: nextLevel,
+        height,
+        y: (y + 60),
+        x: x + step * Math.pow(2, nextLevel)
+      });
     }
 
     return result
 
   }
 
-  getTreeView() {
+  getTreeView(x: number = 800) {
 
     const result: any[] = [];
+    console.log(this.getHeight())
+    const height = this.getHeight();
 
-    return this.addTreeNode({ current: this.root, result })
+    const xs = 0 + 40 * Math.pow(2, height)
+
+    return this.addTreeNode({ current: this.root, result, x: xs, level: height, height });
   }
 }
 
@@ -188,13 +255,36 @@ bst.insert(6);
 bst.insert(35);
 bst.insert(3);
 bst.insert(8);
+bst.insert(9);
+bst.insert(7);
 bst.insert(27);
 bst.insert(55);
 bst.insert(1);
-// bst.insert(3);
+bst.insert(4);
+bst.insert(0);
+bst.insert(2);
+
+
 bst.insert(25);
 bst.insert(29);
 bst.insert(60);
+
+bst.insert(50);
+bst.insert(61);
+bst.insert(59);
+bst.insert(51);
+bst.insert(49);
+bst.insert(28);
+bst.insert(30);
+bst.insert(21);
+bst.insert(26);
+bst.insert(8);
+bst.insert(9);
+
+// bst.insert(100);
+
+
+
 
 export default bst;
 
--- src/widgets/binary-tree/ui/binary-tree.tsx
+++ src/widgets/binary-tree/ui/binary-tree.tsx
@@ -1,30 +1,93 @@
-import { NodeEdge, ArrowMarker } from "@/features/tree-view";
+"use client";
+import { NodeEdge, ArrowMarker, NodeItem } from "@/features/tree-view";
 import tree from "../model/binary-tree";
+import { useLayoutEffect, useRef, useState } from "react";
+import { json } from "stream/consumers";
 
 const radius = 20;
+
+const step = 40;
+const height = 4;
+const items = Math.pow(2, height);
+const width = Math.pow(2, height) * (step + radius);
+console.log({ items });
 
 const getDotLineByRadius = (r: number) => (r * Math.sqrt(2)) / 2;
 const dotLine = getDotLineByRadius(radius);
 export const BinaryTree = () => {
-  console.log(tree.getTreeView());
+  const [treeView, setTreeView] = useState<any[]>([]);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+
+    const width = ref.current.clientWidth;
+
+    setTreeView(tree.getTreeView((width - radius) / 2));
+  }, []);
 
   return (
-    <div className="flex min-h-screen flex-col gap-8 items-center p-24">
+    <div
+      ref={ref}
+      className="flex min-h-screen flex-col gap-8 items-center pt-24"
+    >
       <h1>Binary Search Tree</h1>
 
-      <svg width="800px" height="800px" className="bg-white">
+      <svg width="100%" viewBox="0 0 1450 700" className="bg-white ">
         <ArrowMarker />
-        {tree.getTreeView().map((node) => (
+        {treeView.map((node) => (
           <NodeEdge key={node.value} node={node} />
         ))}
-
-        {/* <NodeEdge node={{ value: 2, x: 400, y: 50 }} />
-        <NodeEdge node={{ value: 1, x: 350, y: 120 }} parentNode={{ value: 2, x: 400, y: 50 }} isLeft />
-        <NodeEdge node={{ value: 3, x: 450, y: 120 }} parentNode={{ value: 2, x: 400, y: 50 }} />
-         */}
+        {/* {Array.from({ length: items / 8 }, (i, k) => k).map((item) => {
+          return (
+            <NodeItem
+              key={item}
+              value={item}
+              x={
+                ((((width / items) * item + 40) * 4 - radius) * 4 - radius) *
+                  8 -
+                radius / 2
+              }
+              y={50}
+              r={radius}
+            />
+          );
+        })}
+        {Array.from({ length: items / 4 }, (i, k) => k).map((item) => {
+          return (
+            <NodeItem
+              key={item}
+              value={item}
+              x={((width / items) * item + 40) * 4 - radius / 2}
+              y={100}
+              r={radius}
+            />
+          );
+        })}
+        {Array.from({ length: items / 2 }, (i, k) => k).map((item) => {
+          return (
+            <NodeItem
+              key={item}
+              value={item}
+              x={((width / items) * item + 40) * 2 - radius / 2}
+              y={150}
+              r={radius}
+            />
+          );
+        })}
+        `
+        {Array.from({ length: items }, (i, k) => k).map((item) => {
+          return (
+            <NodeItem
+              key={item}
+              value={item}
+              x={(width / items) * item + 40}
+              y={200}
+              r={radius}
+            />
+          );
+        })} */}
       </svg>
     </div>
   );
 };
-
-
