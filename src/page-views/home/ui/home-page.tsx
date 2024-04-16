7eda4ba3355ec0bb7c8be9c067af13362e63ea9a
Artem M <artmakatera@gmail.com>
2024-04-16 14:26:10 +0200

    fix main page

--- src/page-views/home/ui/home-page.tsx
+++ src/page-views/home/ui/home-page.tsx
@@ -3,10 +3,10 @@ import { PostCard } from "@/entities/post-card";
 
 export const HomePage = () => {
   return (
-    <main className="flex flex-col items-center px-24 py-10">
+    <main className="flex flex-col items-center px-4 md:px-24 py-10 m-auto">
       <TypographyH1>Explore Algorithms</TypographyH1>
 
-      <div className="pt-4 flex gap-4 w-full">
+      <div className="max-w-6xl pt-4 gap-4 grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <PostCard
           link="/binary-search"
           title="Binary Search"
