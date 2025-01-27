3ad7a6f88163091a4b7b7a191dd30ac688e1af27
Artem M <artmakatera@gmail.com>
2025-01-27 12:21:30 +0100

    add java code

--- src/widgets/binary-tree/model/languages-map-settings/java/traverse.ts
+++ src/widgets/binary-tree/model/languages-map-settings/java/traverse.ts
@@ -1,24 +1,47 @@
 import { LANGUAGES } from "@/shared/constants/languages";
 import { STEPS } from "../../constants";
+export const code = `class BinaryTree {
+  Node root;
 
-
-export const code = ` class BinaryTree {
-
-  constructor() {
-    this.root = null;
+  BinaryTree() {
+    root = null;
   }
 
-}
-`;
+  public List<Integer> bfs() {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
 
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      Node currentNode = queue.poll();
+      result.add(currentNode.value);
+
+      if (currentNode.left != null) {
+        queue.add(currentNode.left);
+      }
+
+      if (currentNode.right != null) {
+        queue.add(currentNode.right);
+      }
+    }
+
+    return result;
+  }
+}`;
 
 export const highlightLines: { [key in STEPS]?: number[] } = {
-  [STEPS.start]: [],
-  [STEPS.addToQueue]: [],
-  [STEPS.popFromQueue]: [],
-  [STEPS.addToResult]: [],
-  [STEPS.endTraverse]:[],
-
+  [STEPS.start]: [8, 31, 9],
+  [STEPS.addToQueue]: [8, 31, 14, 15],
+  [STEPS.addLeftToQueue]: [8, 31,17, 28, 21, 22, 23],
+  [STEPS.addRightToQueue]: [8, 31,17, 28, 25, 26, 27],
+  [STEPS.popFromQueue]: [8, 31,17, 28, 18],
+  [STEPS.addToResult]:  [8, 31,17, 28, 19],
+  [STEPS.endTraverse]: [8, 31, 30],
+  [STEPS.earlyEndTraverse]: [8, 31, 10, 11, 12],
 };
 
 
