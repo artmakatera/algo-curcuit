642a56bb5bd6398eca67d37e271234a268b2f5f9
Artem M <artmakatera@gmail.com>
2025-03-26 14:10:09 +0100

    add description to binary search

--- src/page-views/binary-search/ui/binary-search-page.tsx
+++ src/page-views/binary-search/ui/binary-search-page.tsx
@@ -1,15 +1,37 @@
 'use client'
 import { TypographyH1 } from "@/components/ui/typography";
+import { AlgorithmComplexities } from "@/features/algorithm-complexity";
+import { DescriptionDialog } from "@/features/description-dialog";
+import { COMPLEXITIES } from "@/shared/constants/complexities";
 import {
   BinarySearchVisualize,
   DEFAULT_SORTED_ARRAY,
   createBinarySearchStepSnapshot,
 } from "@/widgets/binary-search-visualize";
+import { SpaceComplexityDescription, TimeComplexityDescription } from "./comlexitiy-descriptions";
+import { DialogInfo } from "./dialog-info";
 
 export function BinarySearchPage() {
   return (
     <main className="flex flex-col px-4 sm:px-24 py-10">
       <TypographyH1 className="self-center">Binary Search</TypographyH1>
+      <div className="m-auto ">
+              <AlgorithmComplexities
+                spaceComplexity={COMPLEXITIES.O_1}
+                timeComplexity={COMPLEXITIES.O_LOG_N}
+                spaceComplexityDescription={<SpaceComplexityDescription />}
+                timeComplexityDescription={<TimeComplexityDescription />}
+              />
+            </div>
+            <DescriptionDialog
+              title="Binary Search"
+              spaceComplexity={COMPLEXITIES.O_1}
+              timeComplexity={COMPLEXITIES.O_LOG_N}
+              spaceComplexityDescription={<SpaceComplexityDescription />}
+              timeComplexityDescription={<TimeComplexityDescription />}
+            >
+              <DialogInfo />
+            </DescriptionDialog>
       <BinarySearchVisualize
         defaultArray={DEFAULT_SORTED_ARRAY}
         createStepSnapshot={createBinarySearchStepSnapshot}
---
+++ src/page-views/binary-search/ui/dialog-info.tsx
@@ -0,0 +1,86 @@
+import { TypographyList, TypographyP } from "@/components/ui/typography";
+import { VisualArrayItem, VisualArrayWrapper } from "@/features/visual-array";
+import { ArrowLeft, ArrowLeftRight } from "lucide-react";
+
+export const DialogInfo = () => {
+  return (
+    <div>
+      <TypographyP>
+        Binary search is an efficient algorithm for finding a target value
+        within a sorted array.
+      </TypographyP>
+      <TypographyP>
+        <strong>In essence:</strong> Binary search repeatedly divides the search
+        interval in half. It compares the middle element of the array to the
+        target value. If they match, the search is complete. If the target value
+        is less than the middle element, the search continues in the left half.
+        If it's greater, the search continues in the right half.
+      </TypographyP>
+      <TypographyP>
+        For instance find 6:
+      </TypographyP>
+      <div className="grid place-items-center">
+        <VisualArrayWrapper className="mt-4 -ml-4">
+          <VisualArrayItem value={1} index={2} />
+          <VisualArrayItem value={2} index={1} />
+          <VisualArrayItem value={3} index={3} />
+          <VisualArrayItem value={4} index={0} />
+          <VisualArrayItem value={5} index={4} />
+          <VisualArrayItem value={6} index={5} />
+          <VisualArrayItem value={7} index={6}  />
+        </VisualArrayWrapper>
+        <VisualArrayWrapper className="mt-4 -ml-4">
+          <VisualArrayItem value={1} index={2} isComparing />
+          <VisualArrayItem value={2} index={1} />
+          <VisualArrayItem value={3} index={3} />
+          <VisualArrayItem value={4} index={0} isChecking />
+          <VisualArrayItem value={5} index={4} />
+          <VisualArrayItem value={6} index={5} />
+          <VisualArrayItem value={7} index={6} isComparing />
+        </VisualArrayWrapper>
+        <VisualArrayWrapper className="mt-4 -ml-4">
+          <VisualArrayItem value={1} index={2} className="opacity-40" />
+          <VisualArrayItem value={2} index={1} className="opacity-40" />
+          <VisualArrayItem value={3} index={3} className="opacity-40" />
+          <VisualArrayItem value={4} index={0} className="opacity-40" />
+          <VisualArrayItem value={5} index={4} isComparing />
+          <VisualArrayItem value={6} index={5} isChecking />
+          <VisualArrayItem value={7} index={6} isComparing />
+        </VisualArrayWrapper>
+        <VisualArrayWrapper className="mt-4 -ml-4">
+          <VisualArrayItem value={1} index={2} className="opacity-40" />
+          <VisualArrayItem value={2} index={1} className="opacity-40" />
+          <VisualArrayItem value={3} index={3} className="opacity-40" />
+          <VisualArrayItem value={4} index={0} className="opacity-40" />
+          <VisualArrayItem value={5} index={4} className="opacity-40" />
+          <VisualArrayItem value={6} index={5} isSorted />
+          <VisualArrayItem value={7} index={6} className="opacity-40" />
+        </VisualArrayWrapper>
+      </div>
+      <TypographyP>Pros:</TypographyP>
+      <TypographyList>
+        <li>
+          <strong>Efficient for Large Datasets:</strong> Efficient for Large Datasets
+        </li>
+        <li>
+          <strong>Simple Implementation (Iterative):</strong>  Relatively easy to understand and implement.
+        </li>
+       
+      </TypographyList>
+      <TypographyP>Cons:</TypographyP>
+      <TypographyList>
+        <li>
+          <strong>Requires Sorted Data: </strong> Only works on sorted arrays.
+        </li>
+        <li>
+          <strong>Slow Performance:</strong> Generally slower than other sorting
+          algorithms.
+        </li>
+        <li>
+          <strong>Limited Practical Use:</strong> Rarely used in practice due to
+          its inefficiency.
+        </li>
+      </TypographyList>
+    </div>
+  );
+};
